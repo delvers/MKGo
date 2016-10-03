4c3c41d774949628ae531732cf7d28ac63e5f0fa
David <davide135@gmail.com>
2016-10-03 19:43:29 +0200

    remove win8.1 support add uwp support

--- MKGo/MKGo.UWP/Properties/AssemblyInfo.cs
+++ MKGo/MKGo.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MKGo.Windows.Windows")]
+[assembly: AssemblyTitle("MKGo.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MKGo.Windows.Windows")]
+[assembly: AssemblyProduct("MKGo.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
